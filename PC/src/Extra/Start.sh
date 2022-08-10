973fb368085a0a57a0bbc77e61c8fd7da83a7a2c
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-08-10 23:33:02 +0200

    Made all commands uncommented

--- PC/src/Extra/Start.sh
+++ PC/src/Extra/Start.sh
@@ -1,9 +1,9 @@
 cd ../../bin/Debug/Extra
-#echo Starte Generierung von ResourceHandler
-#cmake ../../../src/Extra -DCMAKE_BUILD_TYPE=Debug
-#make
-#echo Starte ResourceHandler
-#./ResourceCompiler
+echo Starte Generierung von ResourceHandler
+cmake ../../../src/Extra -DCMAKE_BUILD_TYPE=Debug
+make
+echo Starte ResourceHandler
+./ResourceCompiler
 
 echo Starte Kompilierung Hauptprogramm
 cd ..
